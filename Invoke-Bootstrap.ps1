5b64516335c4ae224a3bb889e3d6a39c007c1841
craiglandis <craiglandis@users.noreply.github.com>
2022-02-04 05:59:29 -0800

    Update Invoke-Bootstrap.ps1

--- Invoke-Bootstrap.ps1
+++ Invoke-Bootstrap.ps1
@@ -11,8 +11,8 @@ $bootstrapScriptFilePath = "$env:TEMP\$bootstrapScriptFileName"
 if (Test-Path -Path $bootstrapScriptFilePath -PathType Leaf)
 {
     $passwordSecureString = ConvertTo-SecureString -String $password -AsPlainText -Force
-    $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$userName", $passwordSecureString)
-    Enable-PSRemoting -Force
+    $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$userName", $passwordSecureString)    
+    Enable-PSRemoting -SkipNetworkProfileCheck -Force
     Invoke-Command -FilePath $bootstrapScriptFilePath -Credential $credential -ComputerName localhost #$env:COMPUTERNAME
     Disable-PSRemoting -Force
 }
