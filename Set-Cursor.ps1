0e86db5008365185a5610f36a4da4744a47b0dd2
craiglandis <craiglandis@users.noreply.github.com>
2022-02-04 10:00:05 -0800

    Update Set-Cursor.ps1

--- Set-Cursor.ps1
+++ Set-Cursor.ps1
@@ -1,4 +1,4 @@
-# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force; \\tsclient\c\onedrive\my\Set-Cursor.ps1
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Force; \\tsclient\c\onedrive\my\Set-Cursor.ps1
 
 if ($PSVersionTable.PSVersion -lt [Version]'6.0')
 {
@@ -85,68 +85,84 @@ if ($isWS22 -or $isWin11 -or $isWin10)
     Invoke-Expression -Command $command
 }
 
-if ((Test-Path -LiteralPath 'HKCU:\SOFTWARE\Microsoft\Accessibility') -ne $true)
-{
-    New-Item 'HKCU:\SOFTWARE\Microsoft\Accessibility' -Force -ErrorAction SilentlyContinue | Out-Null
-}
-New-ItemProperty -LiteralPath 'HKCU:\SOFTWARE\Microsoft\Accessibility' -Name 'CursorColor' -Value 12582656 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-New-ItemProperty -LiteralPath 'HKCU:\SOFTWARE\Microsoft\Accessibility' -Name 'TextScaleFactor' -Value 100 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-New-ItemProperty -LiteralPath 'HKCU:\SOFTWARE\Microsoft\Accessibility' -Name 'CursorType' -Value 6 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-New-ItemProperty -LiteralPath 'HKCU:\SOFTWARE\Microsoft\Accessibility' -Name 'CursorSize' -Value 3 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
 
-if ((Test-Path -LiteralPath 'HKCU:\Control Panel\Cursors') -ne $true)
-{
-    New-Item 'HKCU:\Control Panel\Cursors' -Force -ErrorAction SilentlyContinue | Out-Null
+$accessibilityKeyPaths = @('HKU:\.DEFAULT\SOFTWARE\Microsoft\Accessibility','HKCU:\SOFTWARE\Microsoft\Accessibility')
+
+$accessibilityKeyPaths | ForEach-Object {
+
+    $accessibilityKeyPath = $_
+
+    if ((Test-Path -LiteralPath $accessibilityKeyPath) -ne $true)
+    {
+        New-Item $accessibilityKeyPath -Force -ErrorAction SilentlyContinue | Out-Null
+    }
+    New-ItemProperty -LiteralPath $accessibilityKeyPath -Name 'CursorColor' -Value 12582656 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+    New-ItemProperty -LiteralPath $accessibilityKeyPath -Name 'TextScaleFactor' -Value 100 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+    New-ItemProperty -LiteralPath $accessibilityKeyPath -Name 'CursorType' -Value 6 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+    New-ItemProperty -LiteralPath $accessibilityKeyPath -Name 'CursorSize' -Value 3 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
 }
 
-if ($isWS22 -or $isWin11 -or $isWin10)
-{
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name '(default)' -Value 'Windows Aero' -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'AppStarting' -Value "$cursorsFolder\busy_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Arrow' -Value "$cursorsFolder\arrow_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'ContactVisualization' -Value 1 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Crosshair' -Value "$cursorsFolder\cross_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'CursorBaseSize' -Value 80 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'GestureVisualization' -Value 31 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Hand' -Value "$cursorsFolder\link_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Help' -Value "$cursorsFolder\helpsel_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'IBeam' -Value "$cursorsFolder\ibeam_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'No' -Value "$cursorsFolder\unavail_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'NWPen' -Value "$cursorsFolder\pen_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Person' -Value "$cursorsFolder\person_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Pin' -Value "$cursorsFolder\pin_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Scheme Source' -Value 2 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeAll' -Value "$cursorsFolder\move_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeNESW' -Value "$cursorsFolder\nesw_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeNS' -Value "$cursorsFolder\ns_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeNWSE' -Value "$cursorsFolder\nwse_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeWE' -Value "$cursorsFolder\ew_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'UpArrow' -Value "$cursorsFolder\up_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Wait' -Value "$cursorsFolder\wait_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-}
-else
-{
-    $cursorsFolder = '%SystemRoot%\cursors'
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name '(default)' -Value 'Windows Inverted (extra large)' -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'AppStarting' -Value "$cursorsFolder\wait_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Arrow' -Value "$cursorsFolder\arrow_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'ContactVisualization' -Value 1 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Crosshair' -Value "$cursorsFolder\cross_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'CursorBaseSize' -Value 80 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'GestureVisualization' -Value 31 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Hand' -Value "$cursorsFolder\aero_link_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Help' -Value "$cursorsFolder\help_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'IBeam' -Value "$cursorsFolder\beam_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'No' -Value "$cursorsFolder\no_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'NWPen' -Value "$cursorsFolder\pen_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Scheme Source' -Value 2 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeAll' -Value "$cursorsFolder\move_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeNESW' -Value "$cursorsFolder\size1_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeNS' -Value "$cursorsFolder\size4_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeNWSE' -Value "$cursorsFolder\size2_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'SizeWE' -Value "$cursorsFolder\size3_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'UpArrow' -Value "$cursorsFolder\up_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
-    New-ItemProperty -LiteralPath 'HKCU:\Control Panel\Cursors' -Name 'Wait' -Value "$cursorsFolder\busy_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+$cursorsKeyPaths = @('HKU:\.DEFAULT\Control Panel\Cursors','HKCU:\Control Panel\Cursors')
+
+$cursorsKeyPaths | ForEach-Object {
+
+    $cursorsKeyPath = $_
+
+    if ((Test-Path -LiteralPath $cursorsKeyPath) -ne $true)
+    {
+        New-Item $cursorsKeyPath -Force -ErrorAction SilentlyContinue | Out-Null
+    }
+
+    if ($isWS22 -or $isWin11 -or $isWin10)
+    {
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name '(default)' -Value 'Windows Aero' -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'AppStarting' -Value "$cursorsFolder\busy_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Arrow' -Value "$cursorsFolder\arrow_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'ContactVisualization' -Value 1 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Crosshair' -Value "$cursorsFolder\cross_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'CursorBaseSize' -Value 80 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'GestureVisualization' -Value 31 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Hand' -Value "$cursorsFolder\link_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Help' -Value "$cursorsFolder\helpsel_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'IBeam' -Value "$cursorsFolder\ibeam_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'No' -Value "$cursorsFolder\unavail_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'NWPen' -Value "$cursorsFolder\pen_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Person' -Value "$cursorsFolder\person_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Pin' -Value "$cursorsFolder\pin_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Scheme Source' -Value 2 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeAll' -Value "$cursorsFolder\move_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeNESW' -Value "$cursorsFolder\nesw_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeNS' -Value "$cursorsFolder\ns_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeNWSE' -Value "$cursorsFolder\nwse_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeWE' -Value "$cursorsFolder\ew_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'UpArrow' -Value "$cursorsFolder\up_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Wait' -Value "$cursorsFolder\wait_eoa.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+    }
+    else
+    {
+        $cursorsFolder = '%SystemRoot%\cursors'
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name '(default)' -Value 'Windows Inverted (extra large)' -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'AppStarting' -Value "$cursorsFolder\wait_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Arrow' -Value "$cursorsFolder\arrow_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'ContactVisualization' -Value 1 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Crosshair' -Value "$cursorsFolder\cross_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'CursorBaseSize' -Value 80 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'GestureVisualization' -Value 31 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Hand' -Value "$cursorsFolder\aero_link_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Help' -Value "$cursorsFolder\help_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'IBeam' -Value "$cursorsFolder\beam_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'No' -Value "$cursorsFolder\no_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'NWPen' -Value "$cursorsFolder\pen_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Scheme Source' -Value 2 -PropertyType DWord -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeAll' -Value "$cursorsFolder\move_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeNESW' -Value "$cursorsFolder\size1_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeNS' -Value "$cursorsFolder\size4_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeNWSE' -Value "$cursorsFolder\size2_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'SizeWE' -Value "$cursorsFolder\size3_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'UpArrow' -Value "$cursorsFolder\up_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty -LiteralPath $cursorsKeyPath -Name 'Wait' -Value "$cursorsFolder\busy_il.cur" -PropertyType ExpandString -Force -ErrorAction SilentlyContinue | Out-Null
+    }
 }
 
 Write-Output "Refreshing mouse cursor"
@@ -160,3 +176,5 @@ public static extern bool SystemParametersInfo(
 '@
 $cursorRefresh = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo -PassThru
 $cursorRefresh::SystemParametersInfo(0x0057, 0, $null, 0)
+
+Remove-PSDrive -Name HKU
