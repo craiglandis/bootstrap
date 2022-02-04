b36a1e578e8880a250ccce0e46365f4f5e940c2d
craiglandis <craiglandis@users.noreply.github.com>
2022-02-04 05:41:17 -0800

    Add files via upload

---
+++ Invoke-Bootstrap.ps1
@@ -0,0 +1,23 @@
+param(
+    [string]$userName,
+    [string]$password,
+    [string]$bootstrapScriptUrl
+)
+
+$bootstrapScriptFileName = $bootstrapScriptUrl.Split('/')[-1]
+$bootstrapScriptFilePath = "$env:TEMP\$bootstrapScriptFileName"
+(New-Object Net.Webclient).DownloadFile($bootstrapScriptUrl, $bootstrapScriptFilePath)
+
+if (Test-Path -Path $bootstrapScriptFilePath -PathType Leaf)
+{
+    $passwordSecureString = ConvertTo-SecureString -String $password -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$userName", $passwordSecureString)
+    Enable-PSRemoting -Force
+    Invoke-Command -FilePath $bootstrapScriptFilePath -Credential $credential -ComputerName localhost #$env:COMPUTERNAME
+    Disable-PSRemoting -Force
+}
+else
+{
+    Write-Error "File not found: $bootstrapScriptFilePath"
+    exit 2
+}
