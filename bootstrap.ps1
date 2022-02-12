26eb3cc67a13b6fddc67bf87db82fd9030661705
Craig Landis <craiglandis@users.noreply.github.com>
2022-02-12 11:49:46 -0800

    test

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -62,6 +62,7 @@ process
         {
             Write-PSFMessage -Level Error -Message "Failed: $command" -ErrorRecord $_
         }
+        Write-PSFMessage "`$LASTEXITCODE: $LASTEXITCODE"
     }
 
     function Set-PSFramework
@@ -554,7 +555,10 @@ process
     $powerShellx64MsiFilePath = "$bsPath\$powerShellx64MsiFileName"
     $powerShellx64MsiLogFilePath = "$($powerShellx64MsiFilePath).$(Get-Date -Format yyyyMMddHHmmssff).log"
     Invoke-ExpressionWithLogging -command "(New-Object Net.WebClient).DownloadFile(`'$powerShellx64MsiUrl`', `'$powerShellx64MsiFilePath`')"
+    $ErrorActionPreference = 'SilentlyContinue'
     Invoke-ExpressionWithLogging -command "msiexec.exe /package $powerShellx64MsiFilePath /quiet /L*v $powerShellx64MsiLogFilePath ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 | Out-Null"
+    Write-PSFMessage "`$LASTEXITCODE: $LASTEXITCODE"
+    exit
     # Install PS7 preview version
     $powershellPrerelease = $powershellReleases | Where-Object prerelease -eq $true | Sort-Object -Property id -Descending | Select-Object -First 1
     $powerShellPreviewx64MsiUrl = ($powershellPrerelease.assets | Where-Object {$_.browser_download_url.EndsWith('win-x64.msi')}).browser_download_url | Sort-Object -Descending | Select-Object -First 1
