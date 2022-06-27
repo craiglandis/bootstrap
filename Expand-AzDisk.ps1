086eeb4345ddfbb965c6426e33147ca2ac62a334
Craig Landis <clandis@microsoft.com>
2022-06-26 18:42:52 -0700

    Update 2022-06-26T18:42:52

--- Expand-AzDisk.ps1
+++ Expand-AzDisk.ps1
@@ -33,7 +33,8 @@ if ($diskpart)
 
     $volumes = Get-WmiObject -Class Win32_Volume
     $volumeIndex = 0
-    foreach ($volume in $volumes) {
+    foreach ($volume in $volumes)
+    {
         if ($volume.BootVolume -eq $true)
         {
             $bootVolumeIndex = $volumeIndex
@@ -42,31 +43,31 @@ if ($diskpart)
     }
     $bootVolume = $volumes[$bootVolumeIndex]
     Write-Output "PowerShell $($PSVersionTable.PSVersion.ToString())"
-    $os =Get-WmiObject -Class Win32_OperatingSystem
+    $os = Get-WmiObject -Class Win32_OperatingSystem
     "$($os.Caption) $(([Environment]::OSVersion).Version.ToString())"
     Write-Output "Boot volume index: $bootVolumeIndex, Name: $($bootVolume.Name), Caption: $($bootVolume.Caption), DriveLetter: $($bootVolume.DriveLetter), DeviceID: $($bootVolume.DeviceID)"
-    $capacityBefore = [int]($bootVolume.Capacity/1024/1024/1024)
+    $capacityBefore = [int]($bootVolume.Capacity / 1024 / 1024 / 1024)
     Write-Output "Capacity: $([int]($bootVolume.Capacity/1024/1024/1024)) GB, Free $([int]($bootVolume.FreeSpace/1024/1024/1024)) GB, Used: $([int](($bootVolume.Capacity - $bootVolume.FreeSpace)/1024/1024/1024)) GB"
 
     $diskPartScriptPath = [IO.Path]::GetTempFileName()
     "select volume $bootVolumeIndex" | Out-File -FilePath $diskPartScriptPath -Append -ErrorAction Stop
-    "extend" | Out-File -FilePath $diskPartScriptPath -Append -ErrorAction Stop
+    'extend' | Out-File -FilePath $diskPartScriptPath -Append -ErrorAction Stop
 
     & $env:SystemRoot\System32\diskpart.exe $diskPartScriptPath
     Remove-Item -Path $diskPartScriptPath -ErrorAction SilentlyContinue
 
     $volumes = Get-WmiObject -Class Win32_Volume
     $bootVolume = $volumes[$bootVolumeIndex]
-    $capacityAfter = [int]($bootVolume.Capacity/1024/1024/1024)
+    $capacityAfter = [int]($bootVolume.Capacity / 1024 / 1024 / 1024)
     if ($capacityAfter -gt $capacityBefore)
     {
-        Write-Output "Boot volume successfully expanded"
+        Write-Output 'Boot volume successfully expanded'
         Write-Output "Capacity: $([int]($bootVolume.Capacity/1024/1024/1024)) GB, Free $([int]($bootVolume.FreeSpace/1024/1024/1024)) GB, Used: $([int](($bootVolume.Capacity - $bootVolume.FreeSpace)/1024/1024/1024)) GB"
         exit 0
     }
     else
     {
-        Write-Error "Failed to expand boot volume"
+        Write-Error 'Failed to expand boot volume'
         exit 1
     }
 }
@@ -88,6 +89,7 @@ else
 
     Invoke-ExpressionWithLogging -command "Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force -ErrorAction Stop"
     $vm = Invoke-ExpressionWithLogging -command "Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -ErrorAction Stop"
+    $location = $vm.Location
     $osDiskName = $vm.StorageProfile.OsDisk.Name
     $osDisk = Invoke-ExpressionWithLogging -command "Get-AzDisk -ResourceGroupName $resourceGroupName -DiskName $osDiskName"
     $currentDiskSizeGB = $osDisk.DiskSizeGB
@@ -98,7 +100,8 @@ else
     Update-AzDisk -ResourceGroupName $resourceGroupName -Disk $osDisk -DiskName $osDiskName -ErrorAction Stop
     Invoke-ExpressionWithLogging -command "Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -ErrorAction Stop"
 
-    do {
+    do
+    {
         Start-Sleep -Seconds 3
         $vmStatus = Invoke-ExpressionWithLogging -command "Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Status -ErrorAction Stop"
         $vmAgentStatus = $vmStatus.VMAgent.Statuses.DisplayStatus
@@ -116,7 +119,7 @@ else
     $publisher = 'Microsoft.Compute'
     $extensionType = 'CustomScriptExtension'
     $name = "$publisher.$extensionType"
-    [version]$version = (Get-AzVMExtensionImage -Location $location -PublisherName $publisher -Type $extensionType | sort {[Version]$_.Version} -Desc | select Version -first 1).Version
+    [version]$version = (Get-AzVMExtensionImage -Location $location -PublisherName $publisher -Type $extensionType | Sort-Object {[Version]$_.Version} -Desc | Select-Object Version -First 1).Version
     $typeHandlerVersion = "$($version.Major).$($version.Minor)"
     $result = Set-AzVMExtension -Location $location -ResourceGroupName $resourceGroupName -VMName $vmName -Publisher $publisher -ExtensionType $extensionType -Name $name -Settings $settings -TypeHandlerVersion $typeHandlerVersion
     Write-Output "Set-AzVMExtension: IsSuccessStatusCode: $($result.IsSuccessStatusCode) StatusCode: $($result.StatusCode) ReasonPhrase: $($result.ReasonPhrase)"
