4b369bcb7a39cfc6f81477d05f8632cd8e647ddb
Craig Landis <clandis@microsoft.com>
2023-07-02 10:08:00 -0700

    Update 2023-07-02T10:08:00

--- Set-Wallpaper.ps1
+++ Set-Wallpaper.ps1
@@ -1323,9 +1323,11 @@ switch ($displaySettings) {
 	"3840x2160x100" {$fontSize = 30}
 	"3840x2160x150" {$fontSize = 28}
 	"2560x1440x100" {$fontSize = 16}
+	"2560x1440x125" {$fontSize = 16}
 	"2560x1440x150" {$fontSize = 20}
-	"1920x1200x100" {$fontSize = 16}
-	"1920x1200x125" {$fontSize = 14}
+	"1920x1200x100" {$fontSize = 18}
+	"1920x1200x150" {$fontSize = 14}
+	"1920x1080x100" {$fontSize = 14}
 	Default {$fontSize = 14}
 }
 Out-Log "Font size: $fontSize"
