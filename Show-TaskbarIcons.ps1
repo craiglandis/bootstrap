7b640c1d53f985bd1e9ca021399ebbab4b2cd527
craiglandis <craiglandis@users.noreply.github.com>
2024-05-19 00:27:31 -0700

    Update Show-TaskbarIcons.ps1

--- Show-TaskbarIcons.ps1
+++ Show-TaskbarIcons.ps1
@@ -2,12 +2,15 @@
 # Taskbar reflected changes immediately, don't have to call advapi32.dll to refresh it
 [CmdletBinding(SupportsShouldProcess = $true)]
 param (
-    [System.IO.FileInfo]$path,
-    [string]$someStringParam,
-    [switch]$someSwitchParam,
-    [ValidateSet('Red', 'Green', 'Blue')]
-    [string]$color
+    [switch]$addScheduledTask
 )
+
+$scriptStartTime = Get-Date
+$scriptStartTimeString = Get-Date -Date $scriptStartTime -Format yyyyMMddHHmmss
+$scriptFullName = $MyInvocation.MyCommand.Path
+$scriptFolderPath = Split-Path -Path $scriptFullName
+$scriptName = Split-Path -Path $scriptFullName -Leaf
+$scriptBaseName = $scriptName.Split('.')[0]
 
 $verbose = [bool]$PSBoundParameters['verbose']
 $debug = [bool]$PSBoundParameters['debug']
@@ -54,3 +57,25 @@ if ($verbose -or $debug)
 
 $count = $objects | Where-Object {$_.IsPromoted -eq 1} | Measure-Object | Select-Object -ExpandProperty Count
 Out-Log "$count NotifyIconsettings subkeys updated (IsPromoted set to 1)" -raw
+
+if ($addScheduledTask)
+{
+	Out-Log "Adding scheduled task"
+	$taskName = (Get-Culture).TextInfo.ToTitleCase($scriptBaseName)
+	Invoke-ExpressionWithLogging "Unregister-ScheduledTask -TaskName $taskName -Confirm:`$false -ErrorAction SilentlyContinue" -verboseOnly
+    Out-Log "Registering $taskName task without XML" -verboseonly
+    $executePowerShell = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+    $argument = "-NoLogo -NoProfile -File $scriptFullName"
+    $userId = "$env:userdomain\$env:username"
+    $runLevel = 'Highest'
+    $action = New-ScheduledTaskAction -Execute $executePowerShell -Argument $argument -WorkingDirectory $scriptFolderPath
+    $principal = New-ScheduledTaskPrincipal -UserId $userId -RunLevel $runLevel -LogonType Interactive
+    $settings = New-ScheduledTaskSettingsSet -Compatibility Win8
+    $trigger1 = New-ScheduledTaskTrigger -AtLogOn
+    $trigger2 = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Hours 1)
+    $task = New-ScheduledTask -Action $action -Principal $principal -Settings $settings -Trigger $trigger1, $trigger2
+    Register-ScheduledTask -TaskName $taskName -InputObject $task | Out-Null
+    # Get-ScheduledTask -TaskName $taskName | Format-List *
+    Export-ScheduledTask -TaskName $taskName
+    # Start-ScheduledTask -TaskName $taskName
+}
